256cdd027047aa223cd0d38e9b602cd4d6dd5e17
Taiyoh-SU <103705023+Taiyoh-SU@users.noreply.github.com>
2022-05-16 11:55:51 +0900

    Add comment

--- Propaganda.py
+++ Propaganda.py
@@ -1,3 +1,9 @@
+# ジョークプログラム
+# 検閲（プロパガンダ）ソート
+# コンセプト：O(1)のソートアルゴリズムを作りたい！
+# 内容：リストを渡すと「この入力配列は法的な理由により削除されました。」と表示し，要素を消したソートを返す，要素数0なのでソートされているものとするためO(1)である
+
+
 def censorship_sort(list):
     print("WARNING: This input array was deleted for legal reasons.")
     return []
